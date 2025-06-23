b4c399f305486e5412d320993abf2a983f8e83e7
nagira syunya <241004@std.hi-joho.ac.jp>
2025-06-23 11:15:44 +0900

    できました

--- src/Character.java
+++ src/Character.java
@@ -2,6 +2,11 @@ public abstract class Character implements Creature{
     String name;
     int hp;
 
+    public Character(String name,int hp){
+        this.name = name;
+        this.hp = hp;
+    }
+
     public final boolean isAlive() {
         return hp > 0;
     }
